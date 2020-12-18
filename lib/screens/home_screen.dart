b10c146285a0dae53596552a5ec04aaa872b529e
EslamElezaby <mme367254@gmail.com>
2020-12-18 17:05:09 +0200

    AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Color(0xff333333),
+      appBar: AppBar(
+        backgroundColor: Color(0xff333333),
+        centerTitle: true,
+        elevation: 0.0,
+        title: Container(
+          height: 40,
+          child: Image.asset('assets/netflix_logo/netflixLogo.png'),
+        ),
+        leading: Icon(Icons.menu),
+        actions: [
+          Container(
+            height: 40,
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              'assets/netflix_logo/neflixPP.jpg',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
